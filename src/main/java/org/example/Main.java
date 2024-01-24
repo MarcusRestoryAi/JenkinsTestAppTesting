f6383268da6a93b7598f086321b208b919836dcf
Marcus <marcus.henriksson@restory.ai>
2024-01-24 08:40:16 +0100

    fixed maven version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public Main() {}
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
     }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,2 +1,26 @@
-package com.mycompany.app;public class AppTest {
+package com.mycompany.app;
+
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void testAppConstructor() {
+        Main app1 = new Main();
+        Main app2 = new Main();
+        assertEquals(app1.getMessage(), app2.getMessage());
+    }
+
+    @Test
+    public void testAppMessage()
+    {
+        Main app = new Main();
+        assertEquals("Hello World!", app.getMessage());
+    }
 }
